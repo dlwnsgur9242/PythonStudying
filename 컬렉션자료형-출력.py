48fc8bc6f0c3d2ab19193d6c44699e4892c4d48f
dlwnsgur9242 <dlwnsgur9242@naver.com>
2024-10-05 14:06:34 +0900

    컬렉션 자료형 - 출력 형식

--- 컬렉션자료형-출력.py
+++ 컬렉션자료형-출력.py
@@ -9,3 +9,10 @@ print("리스트형(List):", b)
 print("튜플형(Tuple):", c)
 print("세트형(Set):", d)
 print("딕셔너리형(Dictionary):", e)
+
+# 출력 결과
+# 문자열(String): Soojebi
+# 리스트형(List): [1, 2, 3]
+# 튜플형(Tuple): (1, 2, 3)
+# 세트형(Set): {1, 2, 3}
+# 딕셔너리형(Dictionary): {'s': 1, 'j': 2, 'b': 3}
