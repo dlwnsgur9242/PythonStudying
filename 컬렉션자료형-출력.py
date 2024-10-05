c964226337b1d8e2bdb62f91ef8aa91cde1ed9cc
dlwnsgur9242 <dlwnsgur9242@naver.com>
2024-10-05 14:00:18 +0900

    first commit

---
+++ 컬렉션자료형-출력.py
@@ -0,0 +1,11 @@
+a = "Soojebi"
+b = [1, 2, 3]
+c = (1, 2, 3)
+d = {1, 2, 3}
+e = {'s':1, 'j':2, 'b':3}
+
+print("문자열(String):", a)
+print("리스트형(List):", b)
+print("튜플형(Tuple):", c)
+print("세트형(Set):", d)
+print("딕셔너리형(Dictionary):", e)
